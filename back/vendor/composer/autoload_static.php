c4de1e8ce42bb804e6666f7ebb875f2fdd86fa7b
Alfredo Sabando <alfre.sabando@gmail.com>
2023-02-16 17:19:04 -0300

    Creación metodos y tablas
    Arreglar editar que no edita, y borrar me tira un 404

--- back/vendor/composer/autoload_static.php
+++ back/vendor/composer/autoload_static.php
@@ -320,8 +320,8 @@ class ComposerStaticInit2f391e02279adfafb9c7f6df8f4e6742
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
