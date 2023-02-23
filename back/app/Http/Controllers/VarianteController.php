1b5812086571e6c9cb6b5fb3b8bd5fc7046f987b
Alfredo Sabando <alfre.sabando@gmail.com>
2023-02-23 16:40:54 -0300

    creacion variantes

---
+++ back/app/Http/Controllers/VarianteController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\variantes;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VarianteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\variantes  $variantes
+     * @return \Illuminate\Http\Response
+     */
+    public function show(variantes $variantes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\variantes  $variantes
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(variantes $variantes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\variantes  $variantes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, variantes $variantes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\variantes  $variantes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(variantes $variantes)
+    {
+        //
+    }
+}
