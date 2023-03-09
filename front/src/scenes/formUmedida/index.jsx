90d4bfcf1dedf574f164046ec66ec5a0440cbbf1
Alfredo Sabando <alfre.sabando@gmail.com>
2023-03-09 13:39:35 -0300

    Modal formulario

--- front/src/scenes/formUmedida/index.jsx
+++ front/src/scenes/formUmedida/index.jsx
@@ -1,87 +1,114 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, Dialog, DialogTitle, TextField } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 import "../global/App.css";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const FormUM = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
+  const [open, setOpen] = useState(false);
 
   const handleFormSubmit = (values) => {
-    axios.post('http://localhost:8000/api/crearUni', values)
-      .then(response => {
+    axios
+      .post("http://localhost:8000/api/crearUni", values)
+      .then((response) => {
         console.log(response.data);
+        setOpen(true);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleResetForm = (resetForm) => {
+    resetForm({ values: initialValues });
+  };
+
   {
     /*acá comienza el form */
   }
   return (
     <div className="FormMat">
-      <Header title="Crear Unidad de Medida" subtitle="Crear una nueva Unidad de Medida" />
+      <Header
+        title="Crear Unidad de Medida"
+        subtitle="Crear una nueva Unidad de Medida"
+      />
       <div className="Formulario">
-      <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={initialValues}
-        validationSchema={checkoutSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <Box
-              display="grid"
-              gap="30px"
-              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-              sx={{
-                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-              }}
-            >
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Nombre Unidad"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.nombre}
-                name="nombre"
-                error={!!touched.nombre && !!errors.nombre}
-                helperText={touched.nombre && errors.nombre}
-                sx={{ gridColumn: "span 2" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Abreviatura"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.abreviatura}
-                name="abreviatura"
-                error={!!touched.abreviatura && !!errors.abreviatura}
-                helperText={touched.abreviatura && errors.abreviatura}
-                sx={{ gridColumn: "span 2" }}
-              />
-            </Box>
-            <Box display="flex" justifyContent="end" mt="20px">
-              <Button type="submit" color="secondary" variant="contained">
-                Crear Material
-              </Button>
-            </Box>
-          </form>
-        )}
-      </Formik>
+        <Formik
+          onSubmit={handleFormSubmit}
+          initialValues={initialValues}
+          validationSchema={checkoutSchema}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+            resetForm,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <Box
+                display="grid"
+                gap="30px"
+                gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                sx={{
+                  "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+                }}
+              >
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Nombre Unidad"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.nombre}
+                  name="nombre"
+                  error={!!touched.nombre && !!errors.nombre}
+                  helperText={touched.nombre && errors.nombre}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Abreviatura"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.abreviatura}
+                  name="abreviatura"
+                  error={!!touched.abreviatura && !!errors.abreviatura}
+                  helperText={touched.abreviatura && errors.abreviatura}
+                  sx={{ gridColumn: "span 2" }}
+                />
+              </Box>
+              <Box display="flex" justifyContent="end" mt="20px">
+                <Button type="submit" color="secondary" variant="contained">
+                  Crear Material
+                </Button>
+                <Button
+                  type="button"
+                  color="primary"
+                  variant="contained"
+                  onClick={() => handleResetForm(resetForm)}
+                  sx={{ marginLeft: "10px" }}
+                >
+                  Limpiar campos
+                </Button>
+              </Box>
+            </form>
+          )}
+        </Formik>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Elemento creado correctamente</DialogTitle>
+        </Dialog>
       </div>
     </div>
   );
@@ -90,7 +117,6 @@ const FormUM = () => {
 const checkoutSchema = yup.object().shape({
   nombre: yup.string().required("campo requerido"),
   abreviatura: yup.string().required("campo requerido"),
-
 });
 const initialValues = {
   nombre: "",
@@ -98,4 +124,3 @@ const initialValues = {
 };
 
 export default FormUM;
-
