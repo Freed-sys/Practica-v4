c5a4d24cfee5d673b1a23cfceb7659085d5d82ce
Alfredo Sabando <alfre.sabando@gmail.com>
2023-04-01 19:49:27 -0300

    todas las tablas listas
    gracias al interceptor, se usan todas las tablas y se agregan botones correspondientes

--- front/src/scenes/obras/index.jsx
+++ front/src/scenes/obras/index.jsx
@@ -1,5 +1,5 @@
 import { Box, Typography, useTheme, Button } from "@mui/material";
-import { DataGrid, GridToolbar} from "@mui/x-data-grid";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import { mockDataTeam } from "../../data/dataExample";
 import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
@@ -10,7 +10,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Form from "../formInventario";
 import "../global/App.css";
-
+import clienteAxios from "../../helpers/clienteAxios";
 
 {
   /*estamos rellenando con datos falsos, rellenar con listarMaterial y función map */
@@ -20,13 +20,16 @@ const Obras = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
-
   const [obra, setObra] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/mostrarOrden')
+    clienteAxios
+      .get("/api/mostrarOrden")
       .then((response) => {
-        const obrasConIds = response.data.map((obra, index) => ({...obra, id: index + 1}));
+        const obrasConIds = response.data.map((obra, index) => ({
+          ...obra,
+          id: index + 1,
+        }));
         setObra(obrasConIds);
       })
       .catch((error) => {
@@ -34,90 +37,110 @@ const Obras = () => {
       });
   }, []);
 
-
-
   const columns = [
-    { field: "id", headerName: "Código Orden" },
-  {
-    field: "valor",
-    headerName: "Valor Casa",
-    flex: 1,
-    cellClassName: "name-column--cell",
-    renderCell: (params) => (
-      <Typography color={colors.brown[400]}>$ {params.row.valor}</Typography>
-    ),
-  },
-  {
-    field: "casa",
-    headerName: "Tipo Casa",
-    flex: 1,
-    cellClassName: "name-column--cell",
-    renderCell: (params) => (
-      <Typography color={colors.brown[400]}>{params.row.tipo}</Typography>
-    ),
-  },
-  {
-    field: "material",
-    headerName: "Material",
-    flex: 1,
-    cellClassName: "name-column--cell",
-    renderCell: (params) => (
-      <Typography color={colors.brown[400]}>
-        {params.row.nombre_material}
-      </Typography>
-    ),
-  },
-  {
-    field: "estado",
-    headerName: "Estado Obra",
-    flex: 1,
-    cellClassName: "name-column--cell",
-    renderCell: (params) => (
-      <Typography color={colors.brown[400]}>{params.row.name}</Typography>
-    ),
-  },
-
+    {
+      field: "nombre_cliente",
+      headerName: "Cliente",
+      flex: 1,
+      cellClassName: "name-column--cell",
+      renderCell: (params) => (
+        <Typography color={colors.gray[100]}>
+          {params.row.nombre_cliente}
+        </Typography>
+      ),
+    },
+    {
+      field: "apellidos_cliente",
+      headerName: "Apellidos Cliente",
+      flex: 1,
+      cellClassName: "name-column--cell",
+      renderCell: (params) => (
+        <Typography color={colors.gray[100]}>{params.row.apellidos_cliente}</Typography>
+      ),
+    },
+    {
+      field: "nombre_variante",
+      headerName: "Casa",
+      flex: 1,
+      cellClassName: "name-column--cell",
+      renderCell: (params) => (
+        <Typography color={colors.gray[100]}>
+          {params.row.nombre_variante}
+        </Typography>
+      ),
+    },
+    {
+      field: "estado",
+      headerName: "Estado Obra",
+      flex: 1,
+      cellClassName: "name-column--cell",
+      renderCell: (params) => (
+        <Typography color={colors.gray[100]}>{params.row.name}</Typography>
+      ),
+    },
+    {
+      field: "acciones",
+      headerName: "Acciones",
+      flex: 1,
+      sortable: false,
+      cellClassName: "name-column--cell",
+      renderCell: (params) => (
+        <>
+          <Button variant="contained" color="primary">
+            Editar
+          </Button>
+          <Button variant="contained" color="secondary">
+            Eliminar
+          </Button>
+        </>
+      ),
+    },
   ];
 
   return (
     <Box m="30px" display="grid">
-      <Header title="Obras " subtitle="Maneja el estado de las obras desde aquí"/>
+      <Header
+        title="Obras "
+        subtitle="Maneja el estado de las obras desde aquí"
+      />
       <Box
-       m="40px 0 60px 0"
+        m="40px 0 60px 0"
         height="75vh"
         sx={{
-            "& .MuiDataGrid-root": {
-                border: "none",
-            },
-            "& .MuiDataGrid-cell": {
-                borderBottom: "none",
-            },
-            "& .name-column--cell": {
-                color: colors.green[400]
-            },
-            "& .MuiDataGrid-columnHeaders": {
-                backgroundColor: colors.brown[900],
-                borderBottom: "none",
-            },
-            "& .MuiDataGrid-virtualScroller": {
-                backgroundColor: colors.green[900],
-            },
-            "& .MuiDataGrid-footerContainer":{
-                borderTop: "none",
-                backgroundColor: colors.brown[900],
-            },
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.green[400],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.brown[900],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.green[900],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.brown[900],
+          },
         }}
-        
-        >
-        <DataGrid rows={obra} columns={columns} components={{Toolbar: GridToolbar}}/>
-      </Box>    
+      >
+        <DataGrid
+          rows={obra}
+          columns={columns}
+          components={{ Toolbar: GridToolbar }}
+        />
+      </Box>
       <div className="Boton">
-      <Button type="submit" color="secondary" variant="contained">
-                Crear Obra 
-              </Button>
+        <Button type="submit" color="secondary" variant="contained">
+          Crear Obra
+        </Button>
       </div>
     </Box>
-    
   );
 };
 
