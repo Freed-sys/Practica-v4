288e788c8a2ac4a71ee65d15c51732e89eb593eb
Alfredo Sabando <alfre.sabando@gmail.com>
2023-04-13 22:27:20 -0400

    Create PrivateRoute.js

---
+++ front/src/helpers/PrivateRoute.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import { getToken } from "./helpers/usuario";
+import {  Route,  Navigate } from "react-router-dom";
+
+
+function PrivateRoute({Component, ...rest}) {
+  
+    const [loggedIn, setLoggedIn] = useState(false);
+
+
+ useEffect(() => {
+
+    const token = getToken();
+    if (token) {
+      setLoggedIn(true);
+    }
+ },[])
+
+//if (isLoading) return <h1>Loading...</h1>;
+
+ return (
+        <Route {...rest}
+          render={(props) =>
+            loggedIn ? (
+              <Component {...props} />
+            ) : (
+                <Login setLoggedIn/>
+            )
+          }
+        />
+      )
+
+ 
+}
+
+export default PrivateRoute;
