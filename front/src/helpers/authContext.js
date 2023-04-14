ea9ad14e6d2b63d06abc12f953e93eaa99534f03
Alfredo Sabando <alfre.sabando@gmail.com>
2023-04-13 21:41:27 -0400

    agregar PrivateRoute y Redirect

--- front/src/helpers/authContext.js
+++ front/src/helpers/authContext.js
@@ -1,9 +1,22 @@
 import React, { createContext, useState } from "react";
+import { Routes, Route, Navigate, Redirect } from "react-router-dom";
+
 
 export const AuthContext = createContext({
   loggedIn: false,
   setLoggedIn: () => {},
 });
+
+// export function AuthProvider({ children }) {
+//   const [loggedIn, setLoggedIn] = useState(false);
+
+//   return (
+//     <AuthContext.Provider value={{ loggedIn, setLoggedIn }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// }
+
 
 export function AuthProvider({ children }) {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -13,4 +26,28 @@ export function AuthProvider({ children }) {
       {children}
     </AuthContext.Provider>
   );
+}
+
+
+
+
+
+export function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  )
 }
